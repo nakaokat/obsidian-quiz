ff99d41bca1af7b44444b65635b4f2300a93dd25
Taiki Nakaoka <nakaokataiki@gmail.com>
2023-10-08 22:39:27 +0900

    Add models

---
+++ model.ts
@@ -0,0 +1,28 @@
+// クイズ（暗記のための問題）のモデル
+
+
+// --- types ---
+
+// answer type: string
+type answer = string;
+// question type: string
+type question = string;
+// quiz type: product type of question and answer
+type quiz = {
+	question: question;
+	answer: answer;
+}
+// initial view type:
+type initialView = {
+	kind: 'initialView';
+	quiz : quiz;
+}
+// answer view type:
+type answerView = {
+	kind: 'answerView';
+	quiz : quiz;
+}
+// quiz view : sum type of initial view and answer view
+type quizView = initialView | answerView;
+// export types 
+export type {quiz, quizView, initialView, answerView};
