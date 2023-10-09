4f6326e7a51163acac845c18aac1e4c48e8147a9
Taiki Nakaoka <nakaokataiki@gmail.com>
2023-10-09 12:16:32 +0900

    Add QuizComponent

---
+++ QuizComponent.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let question: string;
+	export let answer: string;
+	let shouldShowAnswer = false;	
+	const toggle = () => {
+		shouldShowAnswer = !shouldShowAnswer;
+	};
+</script>
+
+<div class="quiz-container">
+	<div class="quiz-question">{question}</div>
+	{#if shouldShowAnswer}
+		<div class="quiz-answer">{answer}</div>
+	{/if}
+	<button class="quiz-button" on:click={toggle} >
+		{shouldShowAnswer ? 'Hide Answer' : 'shouldShowAnswer Answer'}
+	</button>
+</div>
+
+<style>
+</style>
