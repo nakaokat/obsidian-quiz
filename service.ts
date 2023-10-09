e223d70504eddc847adbe6b346d24f0fa2e8bfdd
Taiki Nakaoka <nakaokataiki@gmail.com>
2023-10-09 12:17:02 +0900

    Use QuizComponent

--- service.ts
+++ service.ts
@@ -1,5 +1,6 @@
 import { MarkdownPostProcessorContext } from 'obsidian';
 import { quiz } from './model';
+import QuizComponent from './QuizComponent.svelte';
 
 
 const sourceToQuiz = (source: string): quiz => {
@@ -12,31 +13,11 @@ const sourceToQuiz = (source: string): quiz => {
 
 export async function processQuizBlock(source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
 	const quiz = sourceToQuiz(source);
-
-	const questionDiv = el.createDiv();
-	questionDiv.setText(quiz.question);
-	// add css class to questionDiv
-	questionDiv.addClass('quiz-question');
-
-	const answerDiv = el.createDiv({ cls: 'hidden-answer' });
-	answerDiv.setText(quiz.answer);
-	// add css class to answerDiv
-	answerDiv.addClass('quiz-answer');
-
-	// add css class to container element
-	el.addClass('quiz-container');
-
-	const toggleButton = el.createEl('button', { text: 'Show Answer' });
-
-	toggleButton.addEventListener('click', () => {
-		if (answerDiv.classList.contains('hidden-answer')) {
-			// initialView to answerView
-			answerDiv.classList.remove('hidden-answer');
-			toggleButton.innerText = 'Hide Answer';
-		} else {
-			// answerView to initialView
-			answerDiv.classList.add('hidden-answer');
-			toggleButton.innerText = 'Show Answer';
+	new QuizComponent({
+		target: el,
+		props: {
+			question: quiz.question,
+			answer: quiz.answer,
 		}
 	});
 }
